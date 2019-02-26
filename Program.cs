c0af45cce867ea8ad0777b6e2a60dd9e8e280a21
zhan <zjc_9999@126.com>
2019-02-26 21:11:25 +0800

    vscodeC#Test

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace myApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine($"The current time is {DateTime.Now}");
+            Console.WriteLine("gitTest");
+            //
+        }
+    }
+}
